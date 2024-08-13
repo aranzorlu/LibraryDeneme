7739f36b593b7e17b272cc163ad6681394056d6e
aranzorlu <ezkalibra@gmail.com>
2024-08-13 10:09:13 +0300

    s

--- src/LibraryDeneme.Domain/BookStoreDataSeederContributor.cs
+++ src/LibraryDeneme.Domain/BookStoreDataSeederContributor.cs
@@ -19,7 +19,7 @@ public class LibraryDenemeDataSeederContributor
 
     public LibraryDenemeDataSeederContributor(
         IRepository<Book, Guid> bookRepository,
-        IRepository<Shelf,Guid> shelfRepository,
+        IRepository<Shelf, Guid> shelfRepository,
         IAuthorRepository authorRepository,
         AuthorManager authorManager)
     {
@@ -29,29 +29,66 @@ public class LibraryDenemeDataSeederContributor
         _authorManager = authorManager;
     }
 
+
+
+
     public async Task SeedAsync(DataSeedContext context)
     {
         if (await _bookRepository.GetCountAsync() > 0)
         {
-
             return;
         }
 
+        var orwell = await _authorRepository.InsertAsync(
+            await _authorManager.CreateAsync(
+                "George Orwell",
+                new DateTime(1903, 06, 25),
+                "Orwell produced literary criticism and poetry, fiction and polemical journalism; and is best known for the allegorical novella Animal Farm (1945) and the dystopian novel Nineteen Eighty-Four (1949)."
+            )
+        );
 
-          
+        var douglas = await _authorRepository.InsertAsync(
+            await _authorManager.CreateAsync(
+                "Douglas Adams",
+                new DateTime(1952, 03, 11),
+                "Douglas Adams was an English author, screenwriter, essayist, humorist, satirist and dramatist. Adams was an advocate for environmentalism and conservation, a lover of fast cars, technological innovation and the Apple Macintosh, and a self-proclaimed 'radical atheist'."
+            )
+        );
 
-            
-            
-            await _shelfRepository.InsertAsync(
-            new Shelf
+        await _bookRepository.InsertAsync(
+            new Book
             {
-
-                ShelfName = "Sciencefiction1",
-                ShelfType = SType.ScienceFiction,
-
+                AuthorId = orwell.Id, // SET THE AUTHOR
+                Name = "1984",
+                Type = BookType.Dystopia,
+                PublishDate = new DateTime(1949, 6, 8),
+                Price = 19.84f
             },
-            autoSave: true);
+            autoSave: true
+        );
+
+        await _bookRepository.InsertAsync(
+            new Book
+            {
+                AuthorId = douglas.Id, // SET THE AUTHOR
+                Name = "The Hitchhiker's Guide to the Galaxy",
+                Type = BookType.ScienceFiction,
+                PublishDate = new DateTime(1995, 9, 27),
+                Price = 42.0f
+            },
+            autoSave: true
+        );
+    await _shelfRepository.InsertAsync(
+           new Shelf
+           {
+
+               ShelfName = "Sciencefiction1",
+               ShelfType = SType.ScienceFiction,
+
+           },
+           autoSave: true);
 
 
     }
 }
+
